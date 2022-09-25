9ce7bb502bbf8a50b9fb024ef88dfd6f023c9d79
Alexander Gnatyuk <agnatyuk@croc.ru>
2022-09-26 02:11:58 +0300

    core: add cli interface

--- src/pass_generator/core.clj
+++ src/pass_generator/core.clj
@@ -1,11 +1,28 @@
 (ns pass-generator.core
   (:require [pass-generator.password :refer [generate-password]]
-            [pass-generator.clipboard :refer [copy-to-clipboard]])
+            [pass-generator.clipboard :refer [copy-to-clipboard]]
+            [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
+
+
+(def cli-options
+  "CLI options defenition."
+  [["-l" "--length LENGTH" "Password length"
+    :default 8
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 100) "Must be a number between 0 and 100"]]
+   ["-h" "--help"]])
+
 
 (defn -main
   "Password generator app entrypoint."
   [& args]
-  (copy-to-clipboard (generate-password 9)))
-
-(-main)
+  (let [arguments (parse-opts args cli-options)
+        options (:options arguments)
+        help-msg (:summary arguments)]
+    (if (:help options)
+      (println help-msg)
+      (let [password
+            (generate-password (:length options))]
+        (copy-to-clipboard password)
+        (println password)))))
